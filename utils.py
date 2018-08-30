67326a628eec6d92c4d196b4d6ef2af3baeeb589
Niudong-win <1619249966@qq.com>
2018-08-30 21:01:27 +0800

    U

--- utils.py
+++ utils.py
@@ -24,3 +24,21 @@ class Utils(object):
         if not isinstance(arr, (list, tuple, np.ndarray)):
             Utils.print_verify_failed(type(arr))
             raise BaseException("Must be a list like (include:list, tuple, np.ndarray)!")
+
+    @staticmethod
+    def verify_ndarray(arr):
+        if not isinstance(arr, np.ndarray):
+            Utils.print_verify_failed(type(arr))
+            raise BaseException("Must be a np.ndarray type!")
+
+    @staticmethod
+    def verify_ndarray_list(arrs):
+        Utils.verify_list(arrs)
+        for arr in arrs:
+            Utils.verify_ndarray(arr)
+
+    @staticmethod
+    def verify_equal_len(vec1, vec2):
+        if len(vec1) != len(vec2):
+            Utils.print_verify_failed("length are " + str(len(vec1)) + " and "+str(len(vec2)))
+            raise BaseException("Must have same length!")
